733213e6b4d489483715a163e92fb00ddec160d0
harish1907 <nehasharma@tranktechies.com>
2023-12-15 10:28:06 +0530

    updated code

--- admin_app/models.py
+++ admin_app/models.py
@@ -4,9 +4,6 @@ from master_app.models import *
 
 class AdminModel(models.Model):
     user = models.OneToOneField(MyUser, on_delete=models.CASCADE, related_name="admin_user")
-    nse_brk = models.PositiveIntegerField("NSE Brk", default=0)
-    mcx_brk = models.PositiveIntegerField("MCX Brk", default=0)
-    mini_brk = models.PositiveIntegerField("MINI Brk", default=0)
     
     def __str__(self):
         return str(self.user)
